5be4767157810dc631428ce6e805c6bcdb35d91e
dushyant kumar <dushyantvelar@gmail.com>
2026-01-15 16:31:01 +0530

    style the sidebar component

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -77,9 +77,9 @@ const Sidebar = () => {
   };
 
   return (
-    <section className=" md:flex flex-col w-80 h-screen! max-h-135 justify-between  bg-[#171717] text-[#b4b4b4] ">
-      <div className="flex flex-col justify-between w-full h-full">
-        {/* header  */}
+    <section className=" md:flex flex-col w-80  justify-between  bg-[#171717] text-[#b4b4b4] ">
+      <div className="flex flex-col w-full">
+        {/* header  for new chat*/}
         <button
           onClick={createNewChat}
           className=" flex items-center justify-between px-2 py-1.5 m-2 border border-[rgba(255,255,255,0.5)] bg-transparent rounded-[5px] hover:bg-[rgb(180,180,180,0.05)] cursor-pointer"
@@ -93,7 +93,7 @@ const Sidebar = () => {
             <i className="fa-regular fa-pen-to-square text-xl text-white bg-[rgb(255,255,255,0.005)]"></i>
           </span>
         </button>
-
+        {/* history */}
         <div className="overflow-y-scroll overflow-x-hidden">
           <ul className="p-2 w-full m-2">
             {allThreads?.map((thread) => (
@@ -105,19 +105,21 @@ const Sidebar = () => {
                 className="group cursor-pointer p-1 mb-0.5 relative hover:bg-[rgb(180,180,180,0.05)] text-white rounded-lg flex justify-between items-center"
               >
                 {thread.title}
-
-                <i
-                  onClick={()=>deleteThread(thread.threadId)}
-                  className="fa-regular fa-trash-can text-purple-600 hover:text-[#f87171] hidden opacity-0 group-hover:opacity-100 transition-all duration-200"
-                ></i>
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation(); //it stops event bubbling
+                    deleteThread(thread.threadId);
+                  }}
+                >
+                  <i className="fa-regular fa-trash-can text-purple-600 hover:text-[#f87171] hidden opacity-0 group-hover:opacity-100 transition-all duration-200"></i>
+                </button>
               </li>
             ))}
           </ul>
         </div>
-        {/* history */}
       </div>
 
-      {/* sign  */}
+      {/* sign for the ai  */}
       <div className="hover:text-shadow-lg hover:text-shadow-purple-600 p-2.5 border-t text-center border-[rgb(255,255,255,0.5)] text-white bg-[#171717]">
         By DecentAi <i className="fa-solid fa-heart"></i>
       </div>
